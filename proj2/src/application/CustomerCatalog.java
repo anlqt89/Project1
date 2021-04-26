e765971124a2cdae03e5e3d94120a912aa51ab4b
Christian Pantoja <christian.pantojaa@gmail.com>
2021-04-26 00:00:12 -0500

    Update CustomerCatalog.java

--- proj2/src/application/CustomerCatalog.java
+++ proj2/src/application/CustomerCatalog.java
@@ -1,7 +1,10 @@
+package application;
+
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -14,7 +17,7 @@ import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 
 
-public class CustomerCatalog extends Application
+public class reorg extends Application
 {
     private Label item1Catalog; private Label itemSummary1;
     private Label item2Catalog; private Label itemSummary2;
@@ -28,71 +31,138 @@ public class CustomerCatalog extends Application
     Stage window;
     Scene scene, scene2, scene3;
 
+    
+    //Catalog
+  
+   
+    //end catalog
+   
+    //Left menu
+    Button catalogButton;
+    Button loginButton;
+    Button viewOrderButton;
+    Button profileButton;
+    Button cartButton;
+    Button signoutandExit;
+    VBox layout ;
+    //End LeftMenu
+    int leftSceneWidth;
+    int leftSceneHeight;
+    {
+    	
+    	this.loginButton = new Button("Login");
+    	this.viewOrderButton = new Button("View Order");
+    	this.profileButton = new Button("Profile");
+    	this.cartButton = new Button("Cart");
+    	this.catalogButton = new Button("Browse Catalog");
+      this.signoutandExit = new Button("Sign Out & Exit");
+    	this.leftSceneWidth = 600;
+    	this.leftSceneHeight = 800;
+    	
+    	layout = new VBox(20);
+      layout.setPrefWidth(20);
+      layout.getChildren().addAll(loginButton, signoutandExit, catalogButton, viewOrderButton, profileButton, cartButton);
+      layout.setPadding(new Insets(10));
+      layout.getChildren().forEach(node -> node.setStyle("-fx-border-color: black; -fx-border-width: 1px; -fx-pref-width: 130px;"));
+
+      scene = new Scene(layout, 800, 600);
+    }
     public static void main(String[] args)
     {
         launch(args);
     }
 
+    public Scene getScene(String choice) {
+
+        Scene scene = null;
+        
+        if (choice.equals("ViewOrder")) {
+        	  System.out.println("in");
+            VBox ViewOrder = new VBox();
+            Label titleViewOrder = new Label("View Order");
+            ViewOrder.getChildren().addAll(layout, titleViewOrder);
+            Scene newScene = new Scene(ViewOrder, 800, 600);
+            return  newScene;
+        }
+        
+        if (choice.equals("BrowseCataLog")) {
+        	Label catalogLabel = new Label("-Supplier's Catalog-");
+            Label item1Catalog = new Label("Product1 - Member: $3.99\n\t\t  Premium: $1.99");
+            Label item2Catalog = new Label("Product2 - Member: $4.99\n\t\t  Premium: $2.99");
+            Label item3Catalog = new Label("Product3 - Member: $10.99\n\t\t  Premium: $6.99");
+            String description1Catalog = ("Small build, migthy sound. Our most compact\nwireless earbuds deliver crips and powerful\nbeats for your everyday grind.");
+            Label itemSummary1 = new Label(description1Catalog);
+            Label itemSummary2 = new Label(description1Catalog);
+            Label itemSummary3 = new Label(description1Catalog);
+            Button addtoCart1 = new Button("Add to Cart");
+            Button addtoCart2 = new Button("Add to Cart");
+            Button addtoCart3 = new Button("Add to Cart");
+            Button proceedCheckout = new Button("Proceed to Checkout");
+        	           
+            GridPane gridpaneCatalog = new GridPane();
+            gridpaneCatalog.setHgap(10);
+            gridpaneCatalog.setVgap(10);
+            gridpaneCatalog.add(item1Catalog, 30, 0); gridpaneCatalog.add(itemSummary1, 30, 1);
+            gridpaneCatalog.add(item2Catalog, 30, 10); gridpaneCatalog.add(itemSummary2, 30, 11);
+            gridpaneCatalog.add(item3Catalog, 30, 20); gridpaneCatalog.add(itemSummary3, 30, 21);
+            gridpaneCatalog.add(addtoCart1, 31, 0); gridpaneCatalog.add(addtoCart2, 31, 10); gridpaneCatalog.add(addtoCart3, 31, 20);
+            gridpaneCatalog.add(proceedCheckout, 30, 30);
+            
+            checkoutTitle = new Label("-Cart Checkout-");
+            
+            //Change scene
+            proceedCheckout.setOnAction(e -> window.setScene(this.getScene("CheckOut")));
+            GridPane gridpaneCheckout = new GridPane();
+            gridpaneCheckout.setHgap(10);
+            gridpaneCheckout.setVgap(10);
+            gridpaneCheckout.add(checkoutTitle, 30, 10);
+            
+            
+            signoutandExit.setOnAction(e -> window.close());
+            addtoCart1.setOnAction(new AddtoCart1());
+            addtoCart2.setOnAction(new AddtoCart2());
+            addtoCart3.setOnAction(new AddtoCart3());
+            
+            
+            addtoCart1.setOnAction(new AddtoCart1());
+            addtoCart2.setOnAction(new AddtoCart2());
+            addtoCart3.setOnAction(new AddtoCart3());
+            
+            VBox layout2 = new VBox(20);
+            layout2.getChildren().addAll(layout, gridpaneCatalog);
+            scene2 = new Scene(layout2, 800, 800);
+            return scene2;
+        }
+
+        if (choice.equals("CheckOut")) {
+            VBox v = new VBox();
+            Label l = new Label("CheckOut");
+            v.getChildren().addAll(l);
+            Scene CheckOutScene = new Scene(v, 800, 600);
+            return CheckOutScene;
+        }
+        return scene;
+    }
     @Override
     public void start(Stage primaryStage)
     {
         window = primaryStage;
-        Label catalogLabel = new Label("-Supplier's Catalog-");
-        Button catalogButton = new Button("Browse Catalog");
-        Button signoutandExit = new Button("Sign Out & Exit");
-
-        item1Catalog = new Label("Product1 - Member: $3.99\n\t\t  Premium: $1.99");
-        item2Catalog = new Label("Product2 - Member: $4.99\n\t\t  Premium: $2.99");
-        item3Catalog = new Label("Product3 - Member: $10.99\n\t\t  Premium: $6.99");
-        String description1Catalog = ("Small build, migthy sound. Our most compact\nwireless earbuds deliver crips and powerful\nbeats for your everyday grind.");
-        itemSummary1 = new Label(description1Catalog);
-        itemSummary2 = new Label(description1Catalog);
-        itemSummary3 = new Label(description1Catalog);
-        Button addtoCart1 = new Button("Add to Cart");
-        Button addtoCart2 = new Button("Add to Cart");
-        Button addtoCart3 = new Button("Add to Cart");
-        Button proceedCheckout = new Button("Proceed to Checkout");
-
-        catalogButton.setOnAction(e -> window.setScene(scene2));
-        signoutandExit.setOnAction(e -> window.close());
-        addtoCart1.setOnAction(new AddtoCart1());
-        addtoCart2.setOnAction(new AddtoCart2());
-        addtoCart3.setOnAction(new AddtoCart3());
-        proceedCheckout.setOnAction(e -> window.setScene(scene3));
-
-        GridPane gridpaneCatalog = new GridPane();
-        gridpaneCatalog.setHgap(10);
-        gridpaneCatalog.setVgap(10);
-        gridpaneCatalog.add(item1Catalog, 30, 0); gridpaneCatalog.add(itemSummary1, 30, 1);
-        gridpaneCatalog.add(item2Catalog, 30, 10); gridpaneCatalog.add(itemSummary2, 30, 11);
-        gridpaneCatalog.add(item3Catalog, 30, 20); gridpaneCatalog.add(itemSummary3, 30, 21);
-        gridpaneCatalog.add(addtoCart1, 31, 0); gridpaneCatalog.add(addtoCart2, 31, 10); gridpaneCatalog.add(addtoCart3, 31, 20);
-        gridpaneCatalog.add(proceedCheckout, 30, 30);
-
-        checkoutTitle = new Label("-Cart Checkout-");
-
-        GridPane gridpaneCheckout = new GridPane();
-        gridpaneCheckout.setHgap(10);
-        gridpaneCheckout.setVgap(10);
-        gridpaneCheckout.add(checkoutTitle, 30, 10);
-
-        VBox layout = new VBox(20);
-        layout.getChildren().addAll(catalogLabel, catalogButton, signoutandExit);
-        scene = new Scene(layout, 300, 300);
-
-        VBox layout2 = new VBox(20);
-        layout2.getChildren().addAll(layout, gridpaneCatalog);
-        scene2 = new Scene(layout2, 800, 800);
-
-        VBox layout3 = new VBox(20);
-        layout3.getChildren().addAll(gridpaneCheckout);
-        scene3 = new Scene(layout3, 800, 800); 
+       
+        //CataLog Actions
+        catalogButton.setOnAction(e -> window.setScene(this.getScene("BrowseCataLog")));
+        //End CataLog Actions
+        
+        //Order Actions
+        viewOrderButton.setOnAction(e -> window.setScene(this.getScene("ViewOrder")));
+        //End Order Actions
 
         layout.setPadding(new Insets(10));
-
         window.setScene(scene);
         window.setTitle("Store");
         window.show();
+        
+        
+        
     }
 
     class AddtoCart1 implements EventHandler<ActionEvent>
@@ -161,4 +231,5 @@ public class CustomerCatalog extends Application
         }
       }
     }
+   
 }
