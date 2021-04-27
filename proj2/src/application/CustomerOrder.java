25b6e6069b17680a2088175b46dcfd94b0b8faf8
anlqt89 <82059787+anlqt89@users.noreply.github.com>
2021-04-26 20:20:53 -0500

    Update CustomerOrder.java

--- proj2/src/application/CustomerOrder.java
+++ proj2/src/application/CustomerOrder.java
@@ -1,5 +1,7 @@
 package application;
 
-public interface CustomerOrder extends Customer{
+public interface CustomerOrder{
+	public String[][] requestAllOrders(int CustomerID);
+	public String[][] requestOneOrder(int OrderID, int CustomerID);
 
 }
